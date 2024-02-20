133b56c1531f4b1f6b67c46db43225150b1f1510
rajinderrv <rajinderrv@rvtechnologies.com>
2024-02-20 22:22:28 +0530

    Fixed the issue of public layout

--- src/layout/DeveloperPublicLayout.jsx
+++ src/layout/DeveloperPublicLayout.jsx
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
-import Sidebar from '../components/DeveloperSidebar';
-import Navigation from '../components/DeveloperNavigation';
 import { getToken } from '../helper/utlis';
 import { Navigate } from 'react-router-dom';
 const DeveloperPublicLayout = ({ children }) => {
-    const [sidebarwrapper , isSidebarWrapper] = useState(false);
-    const handleSidebar = () => {
-        isSidebarWrapper(!sidebarwrapper)
-    }
+
     const token=getToken("developerToken")
     return (
         <>
-            <div className="dashboard-layout">
-                <Sidebar sidebarwrapper={sidebarwrapper} />
-                <main className='main-wrapper'>
-                    <Navigation onClick={handleSidebar} />
-                    {!token?children:<Navigate to="/developer-dashboard"/>}
-                </main>
-            </div>
+            
+        {!token?children:<Navigate to="/developer-dashboard"/>}
+             
         </>
     );
 };
--- src/layout/PublicLayout.jsx
+++ src/layout/PublicLayout.jsx
@@ -4,20 +4,11 @@ import Navigation from '../components/DeveloperNavigation';
 import { Navigate } from 'react-router-dom';
 import { getToken } from '../helper/utlis';
 const PublicLayout = ({ children }) => {
-    const [sidebarwrapper , isSidebarWrapper] = useState(false);
-    const handleSidebar = () => {
-        isSidebarWrapper(!sidebarwrapper)
-    }
+
     const token=getToken("token")
     return (
         <>
-            <div className="dashboard-layout">
-                <Sidebar sidebarwrapper={sidebarwrapper} />
-                <main className='main-wrapper'>
-                    <Navigation onClick={handleSidebar} />
-                    {!token? children :<Navigate to="/dashboard"/>}
-                </main>
-            </div>
+              {!token? children :<Navigate to="/dashboard"/>}
         </>
     );
 };
