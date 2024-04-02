95acc97db89e70c70190e9b8fc83a86eecbd9c1a
rajinderrv <rajinderrv@rvtechnologies.com>
2024-04-02 14:51:00 +0530

    worked on logout

--- src/redux/slices/clientDataSlice.js
+++ src/redux/slices/clientDataSlice.js
@@ -185,7 +185,11 @@ export function getFolderData(payload, role, callback) {
         dispatch(setScreenLoader())
         try {
 <<<<<<< HEAD
+<<<<<<< HEAD
             let result = await clientInstance.get(generateApiUrl(payload, `${role}/documents`))
+=======
+            let result = await clientInstance.get(generateApiUrl(payload,`${role}/documents`))  
+>>>>>>> e3fb992 (worked on logout)
 =======
             let result = await clientInstance.get(generateApiUrl(payload,`${role}/documents`))  
 >>>>>>> e3fb992 (worked on logout)
