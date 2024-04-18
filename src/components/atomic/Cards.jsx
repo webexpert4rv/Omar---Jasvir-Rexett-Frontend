f3e5bfa43fc9022743bb34a0b2fcf399785d0205
rajinderrv <rajinder@rvtechnologies.com>
2024-04-18 13:27:52 +0530

    Card Design

--- src/components/atomic/Cards.jsx
+++ src/components/atomic/Cards.jsx
@@ -13,42 +13,40 @@ const Cards = ({ item, handleCardClick }) => {
         onClick={() => handleCardClick(item?.developer?.id)}
         key={item?.developer_id}
       >
-        <div className="inner-dev-card">
-          <div className="user-imgbx">
-            <img
-              src={
-                item?.developer?.profile_picture
-                  ? item?.developer?.profile_picture
-                  : userImg
-              }
-              alt="developerImage"
-              className="user-img"
-            />
-          </div>
-          <div className="text-center">
-            <h3 className="user-name">{item?.developer?.name}</h3>
-            <p className="designation-user">
-              {item?.developer?.developer_detail?.professional_title}
-            </p>
-            <p className="email-user">{item?.developer?.email}</p>
-            <ul className="social-icons d-none">
-              <li>
-                <Link to={item?.developer?.developer_detail?.github_url}>
-                  <FaGithub />
-                </Link>
-              </li>
-              <li>
-                <Link to={item?.developer?.developer_detail?.linkedin_url}>
-                  <FaLinkedin />
-                </Link>
-              </li>
-              {/* <li>
-                <Link to={item?.developer?.developer_detail?.email}>
-                  <MdEmail />
-                </Link>
-              </li> */}
-            </ul>
-          </div>
+        <div className="user-imgbx">
+          <img
+            src={
+              item?.developer?.profile_picture
+                ? item?.developer?.profile_picture
+                : userImg
+            }
+            alt="developerImage"
+            className="user-img"
+          />
+        </div>
+        <div className="text-center">
+          <h3 className="user-name">{item?.developer?.name}</h3>
+          <p className="designation-user">
+            {item?.developer?.developer_detail?.professional_title}
+          </p>
+          <p className="email-user">{item?.developer?.email}</p>
+          <ul className="social-icons d-none">
+            <li>
+              <Link to={item?.developer?.developer_detail?.github_url}>
+                <FaGithub />
+              </Link>
+            </li>
+            <li>
+              <Link to={item?.developer?.developer_detail?.linkedin_url}>
+                <FaLinkedin />
+              </Link>
+            </li>
+            {/* <li>
+              <Link to={item?.developer?.developer_detail?.email}>
+                <MdEmail />
+              </Link>
+            </li> */}
+          </ul>
         </div>
       </div>
     </>
