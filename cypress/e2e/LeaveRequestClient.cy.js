d84280f3e0ddf1640056a998b2347924071ddd26
pankaj95 <pankaj@avioxtechnologies.com>
2024-06-28 18:50:38 +0530

    updated code for test cases

--- cypress/e2e/LeaveRequestClient.cy.js
+++ cypress/e2e/LeaveRequestClient.cy.js
@@ -4,18 +4,28 @@ const login = (email, password) => {
   cy.get('input[name="password"]').should('be.visible').type(password)
   cy.get('button[type="submit"]').should('be.visible').click()
 }
-
 const baseUrl = 'http://localhost:3000';
 
 describe('Leave request page', () => {
   it('should navigate to the dashboard after successful login', () => {
     cy.visit('http://localhost:3000')
-    
+
     // Perform login 
-    login("pankajClient@yopmail.com", "Pankaj@0987"); 
+    login("pankajClient@yopmail.com", "Pankaj@0987");
+
     cy.url().should('eq', `${baseUrl}/client/dashboard`);
 
     //Visit Leave request page 
     cy.visit(`${baseUrl}/client/leave-request`)
+
+    // Ensure the tabs are visible
+    cy.get('.application-pills .application-item').should('have.length', 6);
+
+    //Check if there is leaves rejected tab present and click on it!
+    cy.get('.application-pills .application-link').contains('Leaves Rejected').click();
+
+    //Check if there is Public Holiday tab is present
+    cy.get('.application-pills .application-link').contains('Public Holiday').click();
+    
   });
 });
