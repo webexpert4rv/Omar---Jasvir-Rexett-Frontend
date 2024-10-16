7d86c54489841c8a8b369c9e0834dd6e0bc17dfa
Pankaj <pankaj@avioxtechnologies.com>
2024-10-16 15:32:55 +0530

    added functionality for custom question

--- src/components/common/JobPostForm/JobPostStep3.jsx
+++ src/components/common/JobPostForm/JobPostStep3.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Col, Form, Row, Button } from "react-bootstrap";
 import { IoClose } from "react-icons/io5";
 import { FiPlus } from "react-icons/fi";
@@ -74,7 +74,7 @@ const SCREENING_OPTIONS = [
     question_type: "custom",
     question: "",
     responseType: "",
-    // inputType: "",
+    inputType: "",
     ideal_answer: "",
   },
 ];
@@ -107,7 +107,6 @@ const JobPostStep3 = ({
     name: "screening_questions",
   });
 
-
   // const handleOnChange = (item) => {
   //   console.log(item, "event")
   //   setDegree(item)
@@ -123,7 +122,7 @@ const JobPostStep3 = ({
       optionId: opt.optionId,
     });
     {
-      opt?.responseType && append({ responseType: "yes/no" });
+      opt?.responseType && append({ responseType: "radio" });
     }
   };
   const isFieldAlreadyAdded = (optId) => {
@@ -137,6 +136,16 @@ const JobPostStep3 = ({
       return false;
     }
   };
+
+  const handleChangeCustom = (e, idx) => {
+    const { name, value, type, checked } = e.target;
+  
+    const newValue = type === 'checkbox' ? checked : value;
+  
+    setValue(name, newValue);
+  
+  };
+  
   return (
     <div>
       <section className="job-post-section">
@@ -165,6 +174,7 @@ const JobPostStep3 = ({
                       "invalid-field"
                     }`}
                     placeholder="Try asking a question"
+                    onChange={(e) => handleChangeCustom(e, idx)}
                   />
                   <Button
                     variant="transparent"
@@ -197,11 +207,12 @@ const JobPostStep3 = ({
                       //     e.target.value
                       //   )
                       // }
+                      onChange={(e) => handleChangeCustom(e, idx)}
                     >
-                      <option value="yes/no" selected>
+                      <option value="radio" selected>
                         Yes/No
                       </option>
-                      <option value="subjective">Subjective</option>
+                      <option value="input">Subjective</option>
                     </Form.Select>
                   </Form.Group>
                 </Col>
@@ -210,7 +221,7 @@ const JobPostStep3 = ({
                     <Form.Label className="font-14">Ideal answer</Form.Label>
                     <div className="d-flex align-items-center gap-3">
                       {watch(`screening_questions.${idx}.responseType`) ===
-                      "yes/no" ? (
+                      "radio" ? (
                         <>
                           <Controller
                             name={`screening_questions.${idx}.ideal_answer`}
@@ -225,9 +236,10 @@ const JobPostStep3 = ({
                                   label={option.label}
                                   value={option.value}
                                   checked={field.value === option.value}
-                                  onChange={(e) =>
-                                    field.onChange(e.target.value)
-                                  }
+                                  // onChange={(e) =>
+                                  //   field.onChange(e.target.value)
+                                  // }
+                                  onChange={(e) => handleChangeCustom(e, idx)}
                                 />
                               ))
                             }
@@ -242,6 +254,7 @@ const JobPostStep3 = ({
                           type="text"
                           className="common-field font-14"
                           placeholder="Enter Answer"
+                          onChange={(e) => handleChangeCustom(e, idx)}
                         />
                       )}
                     </div>
@@ -268,6 +281,7 @@ const JobPostStep3 = ({
                     className="font-14 job-post-checkbox"
                     id="must2"
                     label="Must have qualification"
+                    onChange={(e) => handleChangeCustom(e, idx)}
                   />
                 </Col>
               </Row>
@@ -371,7 +385,7 @@ const JobPostStep3 = ({
                   <Col md="4" className="mb-md-0 mb-4">
                     <Form.Group>
                       <Form.Label className="font-14">Ideal answer</Form.Label>
-                      {field?.ideal_answer === "Yes" ? (
+                      {field?.ideal_answer === "Yes" || field?.ideal_answer === "yes" || field?.ideal_answer === "no" ? (
                         <Form.Select
                           {...register(
                             `screening_questions.${idx}.ideal_answer`,
